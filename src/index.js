2c1a624a99ebfb6512045f06dbef0e38cb51dfaf
Mahmoud Abdelaziz <mahmoudabdelazizibrahim11@gmail.com>
2024-07-10 22:18:23 +0300

    ➕ Import bootstrap and fontawsome

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+import App from "./App/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
