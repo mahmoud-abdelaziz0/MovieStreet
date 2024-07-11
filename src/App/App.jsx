110734382e2a93091ea5bc4221039b6d04572931
Mahmoud Abdelaziz Ibrahim <157170184+mahmoud-abdelaziz0@users.noreply.github.com>
2024-07-11 18:04:14 +0300

    🧱 Add some page to routing and pass a function a prop to some components

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,33 +1,51 @@
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
-import Layout from "../Components/Layout/Layout"
-import Navbar from "../Components/Navbar/Navbar";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "../Components/Layout/Layout";
 import Home from "../Components/Home/Home";
+import About from "../Components/About/About";
 import Login from "../Components/Login/Login";
 import Register from "../Components/Register/Register";
 import Movies from "../Components/Movies/Movies";
 import People from "../Components/People/People";
 import Tv from "../Components/Tv/Tv";
-
-
+import Profile from "../Components/Profile/Profile";
+import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  function saveUserData() {
+    let encodeingToken = localStorage.getItem("userToken");
+    let decodeingToken = jwtDecode(encodeingToken);
+    console.log("decodeingToken", decodeingToken);
+    setUserData(decodeingToken);
+  }
 
   let routers = createBrowserRouter([
-    {path:"/", element: <Layout/>, children:[
-      // {index: true, element: <Home/>},
-      {path:"home", element: <Home />},
-      {path:"movies", element: <Movies />},
-      {path:"people", element: <People />},
-      {path:"tv", element: <Tv />},
-      {path:"login", element: <Login />},
-      {index: true, element: <Register />},
-    ]}
-  ])
+    {
+      path: "/",
+      element: <Layout userData={userData} />,
+      children: [
+        // {index: true, element: <Home/>},
+        { path: "home", element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "movies", element: <Movies /> },
+        { path: "people", element: <People /> },
+        { path: "tv", element: <Tv /> },
+        { path: "login", element: <Login saveUserData={saveUserData} /> },
+        { path: "profile", element: <Profile userData={userData} /> },
+        { index: true, element: <Register /> },
+      ],
+    },
+  ]);
 
   return (
     <>
-      <RouterProvider router={routers}>
-      </RouterProvider>
+      <RouterProvider router={routers}></RouterProvider>
     </>
   );
 }
