da4c172463a7dabf622bb8facf933cecffdd34df
Mahmoud Abdelaziz Ibrahim <157170184+mahmoud-abdelaziz0@users.noreply.github.com>
2024-07-13 22:12:47 +0300

    📝 ...

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,31 +10,44 @@ import { Link } from "react-router-dom";
 const Navbar = ({ userData, logout }) => {
   return (
     <>
-      <nav className="container-fluid p-3 d-flex flex-md-row flex-column align-items-center justify-content-between">
+      <nav
+        style={{ padding: "10px" }}
+        className=" shadow container-fluid d-flex flex-md-row flex-column align-items-center justify-content-between"
+      >
         <div className="left-nav d-flex flex-md-row flex-column align-items-center ">
           <img
-            src="assets/tablogo3.png"
+            src="assets/moviesLogo2.png"
             alt="logo"
             className="m-0 pe-2"
-            style={{ width: "70px" }}
+            style={{ width: "110px" }}
           />
 
           {userData ? (
             <ul className="list-unstyled d-flex flex-md-row flex-column my-0 ms-5 align-items-center">
               <li className="px-3">
-                <Link to={"home"}>Home</Link>
+                <Link className="hovre" to={"home"}>
+                  Home
+                </Link>
               </li>
               <li className="px-3">
-                <Link to={"about"}>About</Link>
+                <Link className="hovre" to={"about"}>
+                  About
+                </Link>
               </li>
               <li className="px-3">
-                <Link to={"movies"}>Movies</Link>
+                <Link className="hovre" to={"movies"}>
+                  Movies
+                </Link>
               </li>
               <li className="px-3">
-                <Link to={"people"}>People</Link>
+                <Link className="hovre" to={"people"}>
+                  People
+                </Link>
               </li>
               <li className="px-3">
-                <Link to={"tv"}>Tv</Link>
+                <Link className="hovre" to={"tv"}>
+                  Tv
+                </Link>
               </li>
             </ul>
           ) : (
@@ -55,19 +68,25 @@ const Navbar = ({ userData, logout }) => {
             {userData ? (
               <>
                 <li className="px-2 cursor-pointer " onClick={logout}>
-                  <span>LogOut</span>
+                  <span className="hovre">LogOut</span>
                 </li>
                 <li className="px-2">
-                  <Link to={"profile"}>Profile</Link>
+                  <Link className="hovre" to={"profile"}>
+                    Profile
+                  </Link>
                 </li>
               </>
             ) : (
               <>
                 <li className="px-2">
-                  <Link to={"/"}>Register</Link>
+                  <Link className="hovre" to={"/"}>
+                    Register
+                  </Link>
                 </li>
                 <li className="px-2">
-                  <Link to={"login"}>Login</Link>
+                  <Link className="hovre" to={"login"}>
+                    Login
+                  </Link>
                 </li>
               </>
             )}
