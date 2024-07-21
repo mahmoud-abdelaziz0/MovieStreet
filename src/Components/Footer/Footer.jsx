7271c86f3f44a5b8c72224276a3f452e27a5c8a3
Mahmoud Abdelaziz Ibrahim <157170184+mahmoud-abdelaziz0@users.noreply.github.com>
2024-07-21 16:40:56 +0300

    🥅 Change the footer style

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      <h1
-        className=" m-0 p-1 w-100 text-center"
-        style={{ backgroundColor: "#2e3136" }}
-      >
-        Footer
-      </h1>
+      <div className=" m-0 p-1 w-100 text-center">
+        <Link to={"/"}>
+          <img
+            src="assets/moviesLogo2.png"
+            alt="logo"
+            className="my-2"
+            style={{ width: "120px" }}
+          />
+        </Link>
+      </div>
     </>
   );
 };
